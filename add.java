25a5b7fb50296873086507ce6112b0e0ced9f8d8
vamsikarnena <vamsivs0577@gmail.com>
2023-12-12 18:29:32 +0530

    again modified

--- add.java
+++ add.java
@@ -4,8 +4,9 @@ class Sum {
     
     int first = 10;
     int second = 20;
-    int third = 30; 
-    int sum = first + second + third;
+    int third = 30;
+    int fourth = 40; 
+    int sum = first + second + third + fourth;
     System.out.println(+ sum);
   }
 }
