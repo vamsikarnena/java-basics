f57027273be9d67321f8cd753e9f2153ff33b729
vamsikarnena <vamsivs0577@gmail.com>
2023-12-13 17:05:48 +0530

    multiplication in java

--- product.java
+++ product.java
@@ -2,9 +2,9 @@ class Product{
     public static void main(String[] args)
     {
  
-        float f1 = 1.5f;
+        float f3 = 1.5f;
         float f2 = 2.0f;
-        float p = f1 * f2;
+        float p = f3 * f2;
         System.out.println("The product is: " + p);
     }
 }
