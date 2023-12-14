814f6b7800e07544d396519c41b9a9403cb7d593
vamsikarnena <vamsivs0577@gmail.com>
2023-12-14 19:54:49 +0530

    switch program

---
+++ switch.java
@@ -0,0 +1,23 @@
+class switch
+{
+	public static void main(String args[])
+
+	{
+		int n = 2;
+		switch(n)
+		{
+			case 1:
+				System.out.println("monday");
+				break;
+			case 2;
+				System.out.println("tuesday");
+				break;
+		    case 3:
+		    	System.out.println("wednesday");
+		    	break:
+		    	
+		    
+		}
+	}
+
+}
