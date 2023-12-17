0aca2b059c4636e7ae3ff0e40beeb39d70f7e4fc
vamsikarnena <vamsivs0577@gmail.com>
2023-12-17 21:42:10 +0530

    if statement"

---
+++ if.java
@@ -0,0 +1,12 @@
+class If
+{
+	public static void main(String args[])
+	{
+		int x = 20;
+        int y = 18;
+        if (x > y) 
+        {
+            System.out.println("x is greater than y");
+        }
+	} 
+}
