2b2fea2c3c9ca7547ed8cb1ac6e626ebc2af1192
vamsikarnena <vamsivs0577@gmail.com>
2023-12-20 13:43:12 +0530

    if else statement

--- if-else.java
+++ if-else.java
@@ -1,14 +1,10 @@
 class Main {
   public static void main(String[] args) {
     int number = 10;
-
-    // checks if number is greater than 0
     if (number > 0) {
       System.out.println("The number is positive.");
     }
     
-    // execute this block
-    // if number is not greater than 0
     else {
       System.out.println("The number is not positive.");
     }
