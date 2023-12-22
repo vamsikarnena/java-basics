da899d0b520c07b433b80ba6ed2659ab9c1d3e7f
vamsikarnena <vamsivs0577@gmail.com>
2023-12-22 20:02:13 +0530

    narrow casting in java

---
+++ narrowcast.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    double myDouble = 9.77;
+    int myInt = (int) myDouble; // casting double into int; narrow casting.
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+    
+  }
+}
