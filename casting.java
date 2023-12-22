0a088a4463835e2baed46a51e1f39bc408a4555b
vamsikarnena <vamsivs0577@gmail.com>
2023-12-22 19:55:55 +0530

    widening casting in java

---
+++ casting.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // casting int convert into double//
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+  }
+}
