62dece5f69752642ec95fc8e0d8beaad0bf0e7c7
vamsikarnena <vamsivs0577@gmail.com>
2023-12-23 21:35:25 +0530

    math in java

---
+++ math.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.max(5, 10));
+    System.out.println(Math.min(5, 10));  
+    System.out.println(Math.sqrt(49));
+    System.out.println(Math.abs(-55));
+    System.out.println(Math.random());
+}
