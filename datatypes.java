7c00e445ce4dd3c0c4c5483a4ebcc74bc5c0be92
vamsikarnena <vamsivs0577@gmail.com>
2023-12-23 17:06:57 +0530

    datatypes in java

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int myNum = 5;               
+    float myFloatNum = 5.99f;    
+    char myLetter = 'v';     
+    boolean myBool = true;       
+    String myText = "vamsi";     
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}
