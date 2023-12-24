d87e12014bb274f075e63aa008c62aef5e6e70da
vamsikarnena <vamsivs0577@gmail.com>
2023-12-24 18:43:16 +0530

    default ke word in java

---
+++ default.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int day = 4;
+    switch (day) {
+      case 1:
+        System.out.println("Saturday");
+        break;
+      case 2:
+        System.out.println("Sunday");
+        break;
+      default:
+        System.out.println("Weekend");
+    }    
+  }
