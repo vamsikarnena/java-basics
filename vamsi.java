b5850da4b7a0751e0735157473d6990e5ad2b91e
vamsikarnena <vamsivs0577@gmail.com>
2023-12-28 18:54:20 +0530

    vamsi

---
+++ vamsi.java
@@ -0,0 +1,10 @@
+class Vamsi{
+	public String color = "white";
+	public int height = 175;
+	public int wheight = 67;
+
+		public static void main(String[] args) {
+			Vamsi vamsi = new vamsi();
+			System.out.println (vamsi.color);
+		}
+}
