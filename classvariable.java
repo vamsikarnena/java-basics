3ec2a84b9043e18dad52594df8ac98ca5f2b554d
vamsikarnena <vamsivs0577@gmail.com>
2023-12-28 22:00:33 +0530

    class variable in types of variables

---
+++ classvariable.java
@@ -0,0 +1,9 @@
+public class Student {
+	static int id = 546;
+
+	public static void main(String[] args) {
+		Student student = new Student();            // static variable in java these are the globle variable in java
+		System.out.println(student id);
+
+	}
+}
