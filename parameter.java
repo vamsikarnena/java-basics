7e7ca46745c0329f347739305b110798cdff342f
vamsikarnena <vamsivs0577@gmail.com>
2023-12-29 17:46:20 +0530

    parameter variable in java

---
+++ parameter.java
@@ -0,0 +1,12 @@
+public class Demo {
+	public static void main(String[] args) {
+		
+	}
+		public static void test (int height) {
+        height = 177;
+		
+		System.out.println(height);     // inside the parameter only variables can defined
+		
+	}
+	
+}
