102b82da1bc1e65556e2bc8e48fb2db09e79e6b9
vamsikarnena <vamsivs0577@gmail.com>
2023-12-30 22:41:27 +0530

    methods in java

---
+++ methods.java
@@ -0,0 +1,11 @@
+class Main {
+  static void myMethod(String fname) {
+    System.out.println(fname + " good");
+  }
+
+  public static void main(String[] args) {
+    myMethod("vamsi");
+    myMethod("jack");
+    myMethod("tom");
+  }
+}
