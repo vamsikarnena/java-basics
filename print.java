a7d234b0c8332626d0e8438afcad58baecd7d13b
vamsikarnena <vamsivs0577@gmail.com>
2024-01-01 20:51:52 +0530

    printing statements in java

---
+++ print.java
@@ -0,0 +1,11 @@
+class Print {
+    public static void main  (String args[]) {
+        System.out.print ("vamsi");
+        System.out.Print ("karnena");
+        System.out.println ("vamsi");
+        System.out.println ("karnena");
+        System.out.printf ("This is %s and age %d","vamsi",22);
+
+    }
+
+}
