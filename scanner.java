72afd9795fe756827496ba5636bc9438e909bf08
vamsikarnena <vamsivs0577@gmail.com>
2024-01-01 21:59:55 +0530

    scanner in java

---
+++ scanner.java
@@ -0,0 +1,16 @@
+class Vamsi {
+    public static void main (String args []) {
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.println ("what is your name");
+        String name = scanner.nextLine();
+        System.out.println ("how are you");
+        String status = scanner.nextLine();
+        System.out.println("what is your age");
+        int age = scanner.nextInt();
+        System.out.println("thankyou");
+        scanner.close();
+         
+    }
+    
+}
