998a311e340581364cde6bcdade2e49e5c1920c8
vamsikarnena <vamsivs0577@gmail.com>
2024-01-02 21:14:42 +0530

    even or odd program in java

---
+++ even.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main (String args []) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the number");
+        Int number = scanner.nextInt();
+         if(n % 2 == 0) {
+             System.out.println("yes,"+n" it is even number");
+         }   
+          else {
+             System.out.println("yes ,"+n" it is odd number");
+        }    
+    } 
+}
