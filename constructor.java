d3009e9c561d07a613b6404dd157ff3300fc92bc
vamsikarnena <vamsivs0577@gmail.com>
2024-01-03 22:03:58 +0530

    constructor in java

---
+++ constructor.java
@@ -0,0 +1,7 @@
+public class ConstructorPractice {
+    public int i;
+        public static void main (String args[]) {
+            ConstructorPractice cp = new ConstructorPractice();
+            System.out.println(cp.i);
+        }
+}
