61ee9955f3addadbc3592c9410b329c76eb3df31
vamsikarnena <vamsivs0577@gmail.com>
2024-01-03 22:09:52 +0530

    object creation in java

---
+++ object.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public Test () {
+    }
+
+    public Test(int a ) {
+
+    }
+        public static void main (Strijng args []) {
+
+            Test t = new Test();
+            Test t1 = new Test(20);
+            System.out.println(t.hashcode());
+            System.out.println(t1.hashcode());
+            
+             
+    }
+}
