41ea59cfdbd51870856f84983a31b334dd61b2e5
vamsikarnena <vamsivs0577@gmail.com>
2024-01-04 21:54:31 +0530

    java practice

---
+++ practice3.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main (String args[]) {
+        int i;
+       // for(i=0;i<=20;i++) {
+       //     System.out.println("the values are");
+        
+        //   for(i=0;i<=20) {
+            //  System.out.println("the values are");
+          //   i++;
+
+            for(i=0) {
+                if(i<=10) {
+                     System.out.println("the values are");
+                     i++;
+                else 
+                    break;     
+
+                }
+        }
+    }
+ }
