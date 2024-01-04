3f9ab7f1db9b19428f3aa927fad70f3a20d9ea90
vamsikarnena <vamsivs0577@gmail.com>
2024-01-04 21:53:48 +0530

    java practice

---
+++ practice2.java
@@ -0,0 +1,15 @@
+class Test {
+    public static void main (String args[]) {
+        int i = 200;
+        while(i=<500); {
+
+            if(i%2==0) {
+                
+            System.out.println("print i");
+            i++;
+
+            }
+        }
+          
+    }
+}
