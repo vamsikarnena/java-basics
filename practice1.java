27ea19639fc5a9799ba36eb34c58bf5568948ba9
vamsikarnena <vamsivs0577@gmail.com>
2024-01-04 21:52:57 +0530

    looping practice in java

---
+++ practice1.java
@@ -0,0 +1,10 @@
+class Test {
+    public static void main (String args[]) {
+        int i = 1;
+        while(i<=100); {
+            System.out.println("print i");
+            i++;
+        }
+          
+    }
+}
