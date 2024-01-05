da286c3783a367368db116ba4272a2ebf59c37a4
vamsikarnena <vamsivs0577@gmail.com>
2024-01-05 21:19:30 +0530

    array practice in java

---
+++ practice4.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main (String args[]) {
+        int[] j = new int[5];
+        j[0] = 1;
+        j[1] = 46;
+        j[3] = 77;
+        System.out.println(j[3]);
+
+        for(i=0; i<j.len; i++) {
+            System.out.println(j[i]);
+        }
+
+    }
+}
