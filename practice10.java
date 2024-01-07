5c84090ae4808b342cb948951de5cd11958c027a
vamsikarnena <vamsivs0577@gmail.com>
2024-01-07 21:50:54 +0530

    java array practice in java

---
+++ practice10.java
@@ -0,0 +1,12 @@
+public class ForEach {
+    public static void main {
+        int [] a = {1,2,3,4,5,6,7};
+ //       for(i=0; i=a.length; i++) {
+   //         System.out.println(a[i]);           // for each in java
+     //   }
+          for(int value:i) {
+              System.out.println(value);
+          }
+    }
+
+}
