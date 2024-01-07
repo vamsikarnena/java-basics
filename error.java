7d3fd5110c81421ba5d77bfadc10a4a660f8f69d
vamsikarnena <vamsivs0577@gmail.com>
2024-01-07 22:18:48 +0530

    error shows in java

---
+++ error.java
@@ -0,0 +1,11 @@
+class Print {
+    public static void main  (String args[]) {
+        System.err.print ("vamsi");
+        System.err.Print ("karnena");
+        System.err.println ("vamsi");
+        System.err.println ("karnena");
+        System.err.printf ("This is %s and age %d","vamsi",22);
+
+    }
+
+}
