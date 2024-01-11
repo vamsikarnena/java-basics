e74e56fe4653b461bdc71ad96f414697fb575c2e
vamsikarnena <vamsivs0577@gmail.com>
2024-01-11 22:08:00 +0530

    static in java

---
+++ static2.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    static int number;
+
+    public static void main(String args[]) {
+        Test.number = new Random().nextInt();
+
+        System.out.println(Test.number);
+        
+    }
+}
