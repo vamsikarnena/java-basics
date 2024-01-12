4e8a6eb4624a03b4ccc48488e22028e70ac44937
vamsikarnena <vamsivs0577@gmail.com>
2024-01-12 21:33:34 +0530

    var key word in java

---
+++ var.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    public static void main(String args[]){
+
+        var i = 12;
+        var name = "vamsi";
+        var c = i;
+        System.out.println(i);        // without using data type in java use "var " key-word
+        System.out.println(c);
+        System.out.println(iname);
+        var array = new int[1];
+        
+    }
+    
+}
