f85386746aeb647c994c2440a7b69f06af4295b3
vamsikarnena <vamsivs0577@gmail.com>
2024-01-22 21:37:59 +0530

    encapsulation in java

---
+++ encapsulation.java
@@ -0,0 +1,12 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    Person p = new Person();
+    p.setName("John");                 // setter is used in encapsulatin
+    System.out.println(p.getName());   // getter  is used to hide the data
+
+  }
+}
+
+       
